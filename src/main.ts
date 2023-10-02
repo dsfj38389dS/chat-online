9d7651a95d249f9457614e97d3830939eae9e478
dsfj38389dS <sanii_45@mail.ru>
2023-10-02 22:02:17 +0500

    create chat

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as process from 'process';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'static'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
